f86a2c52b2e21db53052e6971c56dfa8f814c913
Artyom Volkov <tricky.fat.cat@gmail.com>
2022-11-02 23:02:33 +0200

    Add base logic

--- Source/TrickyButtons/Private/ButtonBase.cpp
+++ Source/TrickyButtons/Private/ButtonBase.cpp
@@ -2,15 +2,45 @@
 
 
 #include "ButtonBase.h"
+#include "TimelineAnimationComponent.h"
 
 AButtonBase::AButtonBase()
 {
 	PrimaryActorTick.bCanEverTick = true;
+
+	ButtonRootComponent = CreateDefaultSubobject<USceneComponent>("ButtonRoot");
+	SetRootComponent(ButtonRootComponent);
+
+	ButtonAnimationComponent = CreateDefaultSubobject<UTimelineAnimationComponent>("ButtonAnimation");
+}
+
+void AButtonBase::OnConstruction(const FTransform& Transform)
+{
+	switch (InitialState)
+	{
+	case EButtonState::Normal:
+		ButtonAnimationComponent->InitialState = ETimelineAnimationState::Begin;
+		break;
+
+	case EButtonState::Pressed:
+		ButtonAnimationComponent->InitialState = ETimelineAnimationState::End;
+		break;
+
+	default:
+		break;
+	}
+	
+	Super::OnConstruction(Transform);
 }
 
 void AButtonBase::BeginPlay()
 {
 	Super::BeginPlay();
+
+	CurrentState = InitialState;
+	PreviousState = CurrentState;
+
+	ButtonAnimationComponent->OnAnimationFinished.AddDynamic(this, &AButtonBase::ChangeState);
 }
 
 void AButtonBase::Tick(float DeltaTime)
@@ -18,3 +48,68 @@ void AButtonBase::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 }
 
+void AButtonBase::SetIsEnabled(const bool bIsEnabled)
+{
+	CurrentState = bIsEnabled ? PreviousState : EButtonState::Disabled;
+
+	switch (CurrentState)
+	{
+	case EButtonState::Transition:
+		if (ButtonAnimationComponent->GetCurrentState() == ETimelineAnimationState::Pause)
+		{
+			ButtonAnimationComponent->Resume();
+		}
+		break;
+
+	case EButtonState::Disabled:
+		if (ButtonAnimationComponent->GetCurrentState() == ETimelineAnimationState::Transition)
+		{
+			ButtonAnimationComponent->Pause();
+		}
+		break;
+
+	default:
+		break;
+	}
+}
+
+void AButtonBase::Press()
+{
+	switch (CurrentState)
+	{
+	case EButtonState::Normal:
+	case EButtonState::Pressed:
+		CurrentState = EButtonState::Transition;
+		ButtonAnimationComponent->Start();
+		break;
+
+	case EButtonState::Transition:
+		if (bIsReversible)
+		{
+			ButtonAnimationComponent->Reverse();
+		}
+		break;
+
+	default:
+		break;
+	}
+}
+
+void AButtonBase::ChangeState(const ETimelineAnimationState NewAnimationState)
+{
+	PreviousState = CurrentState;
+
+	switch (NewAnimationState)
+	{
+	case ETimelineAnimationState::Begin:
+		CurrentState = EButtonState::Normal;
+		break;
+
+	case ETimelineAnimationState::End:
+		CurrentState = EButtonState::Pressed;
+		break;
+
+	default:
+		break;
+	}
+}
--- Source/TrickyButtons/Public/ButtonBase.h
+++ Source/TrickyButtons/Public/ButtonBase.h
@@ -4,7 +4,19 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "TimelineAnimationComponent.h"
 #include "ButtonBase.generated.h"
+
+class UTimelineAnimationComponent;
+
+UENUM(BlueprintType)
+enum class EButtonState : uint8
+{
+	Normal,
+	Transition UMETA(Hidden),
+	Pressed,
+	Disabled
+};
 
 UCLASS()
 class TRICKYBUTTONS_API AButtonBase : public AActor
@@ -14,10 +26,40 @@ class TRICKYBUTTONS_API AButtonBase : public AActor
 public:	
 	AButtonBase();
 
+	virtual void OnConstruction(const FTransform& Transform) override;
+	
 protected:
 	virtual void BeginPlay() override;
 
 public:	
 	virtual void Tick(float DeltaTime) override;
 
+	UFUNCTION(BlueprintCallable, Category="Button")
+	void SetIsEnabled(const bool bIsEnabled);
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components")
+	USceneComponent* ButtonRootComponent = nullptr;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components")
+	UTimelineAnimationComponent* ButtonAnimationComponent = nullptr;
+	
+	UFUNCTION(BlueprintCallable, Category="Button")
+	void Press();
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Button", meta=(AllowPrivateAccess))
+	EButtonState InitialState = EButtonState::Normal;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Button", meta=(AllowPrivateAccess))
+	EButtonState CurrentState = EButtonState::Normal;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Button", meta=(AllowPrivateAccess))
+	EButtonState PreviousState = EButtonState::Normal;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Button", meta=(AllowPrivateAccess))
+	bool bIsReversible = false;
+
+	UFUNCTION()
+	void ChangeState(const ETimelineAnimationState NewAnimationState);
 };
